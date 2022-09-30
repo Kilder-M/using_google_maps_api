40f81fbc7b399aacedea43fd3eca6a3b37176504
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-09-30 15:00:01 -0300

    feat: :sparkles: Classe de abertura do banco de dados.

---
+++ lib/app/data/db_connection.dart
@@ -0,0 +1,18 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:using_google_maps_api/app/data/script.dart';
+
+class DBConnection {
+  static Database? _db;
+
+  static Future<Database> getConnection() async {
+    if (_db == null) {
+      String path = join(await getDatabasesPath(), 'address_db');
+      return _db =
+          await openDatabase(path, version: 1, onCreate: (db, v) async {
+        await db.execute(createAddressTable);
+      });
+    }
+    return _db!;
+  }
+}
