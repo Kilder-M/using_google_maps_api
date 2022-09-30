2a1c1d4a706dd39a65ee4e3e74e4324d80c3a55b
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-09-30 15:54:02 -0300

    feat: :sparkles: chamando as configuração de injção de dependencias.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:using_google_maps_api/app/utils/setup_injections.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  setupInjection();
   runApp(
     GetMaterialApp(
       title: "Application",
