633c411b134fe7997cb2b53af38e4263d92cc22a
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-09-30 14:59:50 -0300

    feat: :sparkles: script para a criação da tabela de endereço.

---
+++ lib/app/data/script.dart
@@ -0,0 +1,13 @@
+const createAddressTable = '''
+  CREATE TABLE address(
+   id INTEGER NOT NULL PRIMARY KEY
+  ,city CHAR(50) 
+  ,state VARCHAR(100) 
+  ,zip_code CHAR(3) 
+  ,street_address VARCHAR(500) 
+  ,street_address_number CHAR(8) 
+  ,complement VARCHAR(200)
+  ,long CHAR(15)
+  ,lat CHAR(15)
+  )
+ ''';
