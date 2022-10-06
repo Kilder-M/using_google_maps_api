f8c8bc934e1f06a321a3f13ff836c911fdde7e13
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-10-06 18:09:53 -0300

    feat: :construction: Remoção de endereço finalizado.
    Falta fazer refatoração do códigp.

--- lib/app/aplication/modules/home/widgets/home_view_future_address_list.dart
+++ lib/app/aplication/modules/home/widgets/home_view_future_address_list.dart
@@ -1,15 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:using_google_maps_api/app/aplication/modules/home/controllers/home_controller.dart';
 import 'package:using_google_maps_api/app/aplication/modules/home/widgets/home_view_list_tile.dart';
+import 'package:using_google_maps_api/app/widgets/sucess_alert.dart';
 
-Widget homeViewFutureAddressList(HomeController controller) {
+Widget homeViewFutureAddressList(
+  HomeController controller,
+) {
   return Expanded(
     child: FutureBuilder(
       future: controller.getAddressList(),
       builder: ((context, snapshot) {
         if (!snapshot.hasData) {
           return const Center(
-            child: CircularProgressIndicator(),
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+              color: Colors.black,
+              backgroundColor: Colors.grey,
+            ),
           );
         } else if (snapshot.hasError) {
           return const Center(
@@ -30,17 +40,58 @@ Widget homeViewFutureAddressList(HomeController controller) {
                     ),
                   ),
                 )
-              : ListView.builder(
-                  itemCount: controller.addressList.length,
-                  itemBuilder: ((context, index) {
-                    var address = controller.addressList[index];
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
-                      child: homeViewListTile(
-                        title: address.address,
-                      ),
-                    );
-                  }),
+              : Obx(
+                  () => ListView.builder(
+                    itemCount: controller.addressList.length,
+                    itemBuilder: ((context, index) {
+                      var address = controller.addressList[index].obs;
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+                        child: homeViewListTile(
+                          title: address.value.address,
+                          onTap: () async {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text(
+                                      'Deseja excluir esse endereço ? ',
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        child: const Text('Não'),
+                                        onPressed: () {
+                                          Get.back();
+                                        },
+                                      ),
+                                      TextButton(
+                                        child: const Text('Sim'),
+                                        onPressed: () async {
+                                          Get.back();
+                                          await controller
+                                              .removeAddress(address.value);
+                                          Timer(
+                                              const Duration(
+                                                  milliseconds: 1300),
+                                              () => Get.back());
+                                          showDialog(
+                                            context: context,
+                                            builder: ((context) {
+                                              return const SucessAlert(
+                                                  title:
+                                                      'Endereço excluído com Sucesso ! ');
+                                            }),
+                                          );
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                });
+                          },
+                        ),
+                      );
+                    }),
+                  ),
                 );
         }
       }),
